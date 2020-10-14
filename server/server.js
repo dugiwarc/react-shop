cc5d411310555ebccb54fe7ee86ab97a60fee0b2
dugiwarc <gicubotnaru@gmail.com>
2020-10-14 22:30:47 +0200

    add bcrypt; users seed data

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import connectDatabase from "./config/db.js";
 import products from "./data/products.js";
+import colors from "colors";
 
 dotenv.config();
 
 const app = express();
+
+connectDatabase();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -25,5 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.blue.bold
+  )
 );
